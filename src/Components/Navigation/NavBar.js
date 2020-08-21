ffaace0d4196f8e0a09ab0c97068a48d79b8a1e0
thibui0908 <thi.t.bui@sjsu.edu>
2020-08-21 00:55:49 -0700

    Acceptable header

--- src/Components/Navigation/NavBar.js
+++ src/Components/Navigation/NavBar.js
@@ -1,29 +1,27 @@
 import React from "react";
-import { Navbar, NavbarBrand, NavItem, Nav, Collapse} from 'reactstrap'
-import { NavLink, } from "react-router-dom";
-import './NavBar.css';
+import { Navbar, NavbarBrand, NavItem, Nav} from "reactstrap";
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
 
 export default function NavBar(props) {
   const componentsOnNavBar = [
-    { title: 'Home', path: "/" },
-    { title: 'About', path: "/about" },
+    { title: "About", path: "/about" },
+    { title: "Home", path: "/" },
   ];
-    return (
-      <div className="nav-bar">
-        <Navbar>
-          <NavbarBrand>NEED</NavbarBrand>
-          <Collapse navbar>
-            <Nav className="nav-thi ml-auto d-flex align-items-end" navbar>
-              {componentsOnNavBar.map((link, key) => {
-                return (
-                  <NavItem key={key}>
-                    <NavLink id='nav-link-text' to={link.path}>{link.title}</NavLink>
-                  </NavItem>
-                );
-              })}
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
-    );
+  return (
+    <Navbar className="nav-bar">
+      <NavbarBrand className="navbrand">NEED</NavbarBrand>
+      <Nav className="nav-item align-items-end ml-auto">
+        {componentsOnNavBar.map((link, key) => {
+          return (
+            <NavItem key={key}>
+              <NavLink id="nav-link-text" to={link.path}>
+                {link.title}
+              </NavLink>
+            </NavItem>
+          );
+        })}
+      </Nav>
+    </Navbar>
+  );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Container } from 'reactstrap';
-import NavBar from "../Components/Navigation/NavBar";
+import './Home.css';
+
 const Home = () => {
   return (
-    <Container>
+    <Container fluid>
+      <img src="https://www.solidbackgrounds.com/images/website/950x534/950x534-little-green-leaves-free-website-background-image.jpg"></img>
       <h1>Home</h1>
       <p>You are the best :'D</p>
     </Container>
